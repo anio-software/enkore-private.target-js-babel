09568212864a6af56fac7596bdc78932c02d69c1
eXory2024 <exory@anio.software>
2025-07-06 00:30:53 +0200

    Add globalRecordsSymbolForIdentifier and initializedGlobalRecordsSymbolForIdentifier.

--- project/src/constants.ts
+++ project/src/constants.ts
@@ -1,6 +1,9 @@
 export const oldSymbolForIdentifier = "@enkore/js-runtime/globalRecords"
 export const oldSymbolForInitializedGlobalRecords = "@enkore/js-runtime/initializedGlobalRecords"
 
+export const globalRecordsSymbolForIdentifier = "@anio-software/enkore.js-runtime/globalRecords"
+export const initializedGlobalRecordsSymbolForIdentifier = "@anio-software/enkore.js-runtime/initializedGlobalRecords"
+
 export const freezeObjectHelperMethodName = "__enkoreJSRuntimeFreezeObject"
 export const freezeGlobalDataRecordMethodName = "__enkoreJSRuntimeFreezeGlobalDataRecord"
 export const initMethodName = "__enkoreJSRuntimeInitialize"
